12f124b9c5a263bb8852e45d7436cc5c796b86cc
Sporif <Sporif@users.noreply.github.com>
2017-08-20 14:25:59 +0100

    Add new hook

---
+++ hooks/chrome-webstore.js
@@ -0,0 +1,17 @@
+// Add css to Chrome Webstore
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
+	if(['loading', 'complete'].indexOf(changeInfo.status) > -1 & tab.url.startsWith('https://chrome.google.com/webstore')){
+		var css =`
+		/* Place your css here, in between the backticks */
+		`;
+		var addCss = 
+		`if(!cssText){ 
+			var cssText = document.createElement('style'); 
+			cssText.setAttribute('description', 'Added by chrome-webstore.js'); 
+		} 
+		cssText.textContent = \`${css}\`;
+		document.head.appendChild(cssText);`;
+		chrome.tabs.executeScript(tab.id, {code: addCss, allFrames: true});
+	}
+});
